d9c96e87ded01100e9b107d6086263775eb6f06a
Mathew Bramson <mathewbramson@gmail.com>
2017-08-06 23:01:02 -0400

    fix inconsistent test naming

--- template/$PROJECT_NAME$/test/$PROJECT_NAME$/account/user_token_service_test.exs
+++ template/$PROJECT_NAME$/test/$PROJECT_NAME$/account/user_token_service_test.exs
@@ -4,13 +4,13 @@ defmodule <%= @project_name_camel_case %>.Account.UserTokenServiceTest do
   alias <%= @project_name_camel_case %>.Account.UserTokenService
 
   describe "generate_token/2" do
-    test "returns a token if the email is associated with an email" do
+    test "returns a token if the email is associated with a user" do
       user = insert(:user)
       {:ok, token} = UserTokenService.generate_token(user.email, :email_confirmation)
       assert token.user.id == user.id
     end
 
-    test "returns error tuple when no user exists with that email" do
+    test "returns an error tuple when no user exists with that email" do
       result = UserTokenService.generate_token("non-existent-email", :email_confirmation)
       assert result == {:error, :no_user_with_email}
     end
@@ -21,19 +21,19 @@ defmodule <%= @project_name_camel_case %>.Account.UserTokenServiceTest do
   end
 
   describe "verify_token/2" do
-    test "returns tuple containing token if it exists" do
+    test "returns a tuple containing token if it exists" do
       user_token = insert(:user_token, %{type: "email_confirmation"})
       {:ok, resulting_token} = UserTokenService.verify_token(user_token.token, :email_confirmation)
       assert resulting_token.id == user_token.id
       assert resulting_token.user.id == user_token.user.id
     end
 
-    test "returns error tuple if token does not exist" do
+    test "returns an error tuple if token does not exist" do
       result = UserTokenService.verify_token("non-existent-token", :email_confirmation)
       assert {:error, :token_not_found} == result
     end
 
-    test "returns error tuple if token exists but is wrong type" do
+    test "returns an error tuple if token exists but is wrong type" do
       user_token = insert(:user_token, %{type: "email_confirmation"})
       result = UserTokenService.verify_token(user_token.token, :wrong_type)
       assert {:error, :token_not_found} == result
