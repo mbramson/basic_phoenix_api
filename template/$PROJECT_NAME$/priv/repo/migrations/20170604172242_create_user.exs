f1198e9c7497613a0cb035f1f6a45c64b78bbcd5
Mathew Bramson <mathewbramson@gmail.com>
2017-08-17 22:50:09 -0400

    Name migrations consistently
    Make default users table "user" not "account_users"

--- template/$PROJECT_NAME$/priv/repo/migrations/20170604172242_create_user.exs
+++ template/$PROJECT_NAME$/priv/repo/migrations/20170604172242_create_user.exs
@@ -2,7 +2,7 @@ defmodule <%= @project_name_camel_case %>.Repo.Migrations.Create<%= @project_nam
   use Ecto.Migration
 
   def change do
-    create table(:account_users) do
+    create table(:users) do
       add :name, :string, null: false
       add :email, :string, null: false
       add :password_hash, :string, null: false
