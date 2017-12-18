cc9f735fd54f975363183d2784c57828f6fb2e39
Mathew Bramson <mathewbramson@gmail.com>
2017-12-17 21:47:00 -0500

    Add authorization allowed header to CORS

--- template/$PROJECT_NAME$/lib/$PROJECT_NAME$_web/cors.ex
+++ template/$PROJECT_NAME$/lib/$PROJECT_NAME$_web/cors.ex
@@ -2,7 +2,7 @@ defmodule <%= @project_name_camel_case %>Web.CORS do
   use Corsica.Router,
     origins: ["http://localhost:3000", ~r{^https?://(.*\.?)\.com$}],
     allow_credentials: true,
-    allow_headers: ["content-type"],
+    allow_headers: ["content-type", "authorization"],
     log: [rejected: :debug, invalid: :debug, accepted: :debug],
     max_age: 600
 
