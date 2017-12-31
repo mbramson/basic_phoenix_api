f15dfd354bf55d618d83fbbb0f8b073b9207e2ea
Mathew Bramson <mathewbramson@gmail.com>
2017-12-30 19:39:11 -0500

    Fix incorrect types

--- template/$PROJECT_NAME$/lib/$PROJECT_NAME$/types.ex
+++ template/$PROJECT_NAME$/lib/$PROJECT_NAME$/types.ex
@@ -2,6 +2,6 @@ defmodule <%= @project_name_camel_case %>.Types do
   @moduledoc false
 
   # Ecto Schema Types
-  @type user :: <%= @project_name_camel_case %>.User
-  @type user_token :: <%= @project_name_camel_case %>.UserToken
+  @type user :: <%= @project_name_camel_case %>.Account.User
+  @type user_token :: <%= @project_name_camel_case %>.Account.UserToken
 end
